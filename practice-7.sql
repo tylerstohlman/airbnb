6e41ae37bc94561b1831afe71a0c2fe83c63268d
tylerstohlman <tyler.stohlman@kellogg.northwestern.edu>
2022-01-14 23:20:47 +0000

    Practice 7 Done

--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,7 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
-
+SELECT listings.property_type, COUNT(*), MAX(reviews.date_reviewed)
+FROM reviews
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.property_type;
